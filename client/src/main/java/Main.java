2d91a3229342da19fc52f1c5a0177c5b07524bab
DanilovMikhail <danilovmikhails@gmail.com>
2019-11-03 12:28:27 +0300

    1. Добавлена структура проекта 2. Добавлен JavaFX

---
+++ client/src/main/java/Main.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static Stage primaryStage;
+
+    private static void setPrimaryStage(Stage stage)
+    {
+        primaryStage = stage;
+    }
+
+    public static Stage getPrimaryStage()
+    {
+        return Main.primaryStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("rCloud");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
