fdbad106119712e87a6b83ed279e4d8bee9c0b5a
DanilovMikhail <danilovmikhails@gmail.com>
2019-11-17 17:56:02 +0300

    Добавлен класс описание передаваемых объектов

--- common/src/main/java/net/danilovms/rcloud/common/PackageFile.java
+++ common/src/main/java/net/danilovms/rcloud/common/PackageFile.java
@@ -4,11 +4,13 @@ public class PackageFile {
     private int number;
     private String fileName;
     private byte data;
+    private boolean echo;
 
-    public PackageFile(int number, String fileName, byte data){
+    public PackageFile(int number, String fileName, byte data, boolean echo){
         this.number     = number;
         this.fileName   = fileName;
         this.data       = data;
+        this.echo       = echo;
     }
 
     public int getNumber() {
@@ -17,5 +19,14 @@ public class PackageFile {
 
     public String getFileName() {
         return fileName;
+    }
+
+    public boolean isEcho() {
+        return echo;
+    }
+
+    public void setEcho() {
+        this.echo   = true;
+        this.data   = 0;
     }
 }
